1a2f2f8b4082c08d78700814c52e2876ac3ced66
Manos Gouvrikos <manolisgouvrikos@gmail.com>
2020-03-02 01:11:13 +0200

    Updates

---
+++ precache-manifest.ffc28b6e888749bc8380ed8b57e89b24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce75deef872a204711e7dffceeffba08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f6640004f5b8b7065c4",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "eb32da0b9bdbef553118",
+    "url": "/static/css/main.eb8f6c45.chunk.css"
+  },
+  {
+    "revision": "4f6640004f5b8b7065c4",
+    "url": "/static/js/2.c735d4e0.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.c735d4e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb32da0b9bdbef553118",
+    "url": "/static/js/main.b21bbd7b.chunk.js"
+  },
+  {
+    "revision": "5db6dfe7a8e2b4135dd2",
+    "url": "/static/js/runtime-main.aa739829.js"
+  },
+  {
+    "revision": "08b883b9c358b47de686fe5f8cdff31f",
+    "url": "/static/media/profile_pic.08b883b9.jpg"
+  }
+]);
